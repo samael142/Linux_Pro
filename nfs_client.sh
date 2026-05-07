68a17342a1fc323a5067f2ae1c321c67ed8330cb
Sergey Filippenkov <72431391+samael142@users.noreply.github.com>
2026-05-07 09:02:21 +0300

    Create nfs_client.sh

---
+++ nfs_client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NFS_SERVER="127.0.0.1"
+
+apt update && apt install nfs-common -y
+mkdir -p /mnt/nfs_client
+echo "${NFS_SERVER}:/srv/nfs_share /mnt/nfs_client nfs _netdev,auto,nofail,vers=3 0 0" >> /etc/fstab
+systemctl daemon-reload
+mount -a
